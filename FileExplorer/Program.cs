0c5fd9d932b2fb9f250f558227ff847c86cccf58
dbaleeds <meddbat@leeds.ac.uk>
2018-12-17 16:55:43 +0000

    Initial commit

---
+++ FileExplorer/Program.cs
@@ -0,0 +1,145 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using ZetaLongPaths;
+
+using System.DirectoryServices;
+
+using System.DirectoryServices.AccountManagement;
+
+namespace FileExplorer
+{
+    class Program
+    {
+
+        static string directoryInfo(string Dir)
+        {
+            string stringDir = "";
+
+            var di = new DirectoryInfo(Dir);
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                Console.WriteLine(dir.FullName + "<br/>");
+                DirectorySecurity ds = dir.GetAccessControl(AccessControlSections.Access);
+                foreach (FileSystemAccessRule fsar in ds.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount)))
+                {
+                    string userName = fsar.IdentityReference.Value;
+                    string userRights = fsar.FileSystemRights.ToString();
+                    string userAccessType = fsar.AccessControlType.ToString();
+                    string ruleSource = fsar.IsInherited ? "Inherited" : "Explicit";
+                    string rulePropagation = fsar.PropagationFlags.ToString();
+                    string ruleInheritance = fsar.InheritanceFlags.ToString();
+                    stringDir = stringDir + "+ " + userName + " : " + userAccessType + " : " + userRights + " : " + ruleSource + " : " + rulePropagation + " : " + ruleInheritance + "|";
+                }
+            }
+            return stringDir;
+        }
+
+
+
+        static string ToBytesCount(long bytes)
+        {
+            int unit = 1024;
+            if (bytes < unit) return bytes + " KB";
+            int exp = (int)(Math.Log(bytes) / Math.Log(unit));
+            return string.Format("{0:##.##} {1}B", bytes / Math.Pow(unit, exp), "KMGTPE"[exp - 1]);
+        }
+
+        static long DirSize(ZlpDirectoryInfo d)
+        {
+            long size = 0;
+            // Add file sizes.
+            ZlpFileInfo[] fis = d.GetFiles();
+            foreach (ZlpFileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            // Add subdirectory sizes.
+            ZlpDirectoryInfo[] dis = d.GetDirectories();
+            foreach (ZlpDirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+            }
+            return size;
+        }
+
+        static void DirSearch(List<Csv> csvFile, string sDir, int Deep)
+        {
+            
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir, "*", SearchOption.TopDirectoryOnly))
+                {
+                    /*security*/
+                    DirectoryInfo dInfo = new DirectoryInfo(d);
+                    DirectorySecurity dSecurity = dInfo.GetAccessControl();
+                    AuthorizationRuleCollection acl = dSecurity.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+                    StringBuilder accessTxt = new StringBuilder();
+                    foreach (FileSystemAccessRule ace in acl)
+                    {
+                        accessTxt.Append(ace.IdentityReference.Value);
+                        accessTxt.Append(" : ");
+                    }
+
+                    if (accessTxt.Length > 2) {
+                        accessTxt.Length = accessTxt.Length - 2;        
+                    }
+
+                    /* get size */ 
+                    long length = DirSize(new ZetaLongPaths.ZlpDirectoryInfo(d));
+
+                    /* add to CSV file */
+                    csvFile.Add(new Csv(d, ToBytesCount(length), Directory.GetLastAccessTime(d), Directory.GetLastWriteTime(d), accessTxt.ToString()));
+
+                    Console.WriteLine(d + ToBytesCount(length).ToString() + Directory.GetLastAccessTime(d).ToString() + Directory.GetLastWriteTime(d) + accessTxt.ToString());
+
+                    if (Deep > 0) {
+                        DirSearch(csvFile, d, Deep - 1);
+                    }
+
+                }
+            }
+            catch (UnauthorizedAccessException) { }
+        }
+
+        internal class Csv
+        {
+            public Csv(string _Name, string _size, DateTime _lastAccessed, DateTime _lastWriteTime, string _access) {
+                Name = _Name;
+                Size = _size;
+                LastWriteTime = _lastWriteTime;
+                Access = _access;
+            }
+            public string Name { get; set; }
+            public string Size { get; set; }
+            public DateTime LastAccessed { get; set; }
+            public DateTime LastWriteTime { get; set; }
+            public string Access { get; set; }
+            /*public int Age { get; set; }*/
+        }
+
+        static void WriteCsvFile(string filename, IEnumerable<Csv> csv)
+        {
+            TextWriter textWriter = File.CreateText(filename);
+            var csvWriter = new CsvWriter(textWriter);
+            csvWriter.WriteRecords(csv);
+            textWriter.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            string path = "M:\\Work\\Projects\\Code\\Tools";
+            var filename = Directory.GetCurrentDirectory() + @"\file.csv";
+            List<Csv> CsvFile = new List<Csv>();
+            DirSearch(CsvFile, path, 1);
+            WriteCsvFile(filename, CsvFile);
+            Console.WriteLine("here");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FileExplorer/TestStuff/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace TestStuff
+{
+    class Program
+    {
+        static void GetDirectorySecurity(string dir, int levels)
+        {
+            int curLevel = 1;
+            string[] dirs = Directory.GetDirectories(dir);
+            foreach (string directory in dirs)
+            {
+                Console.WriteLine("---------------------------------------------------------");
+                Console.WriteLine(directory);
+                try
+                {
+                    string tabs = "\t";
+                    DirectoryInfo dInfo = new DirectoryInfo(directory);
+                    DirectorySecurity dSecurity = dInfo.GetAccessControl();
+                    AuthorizationRuleCollection acl = dSecurity.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+                    foreach (FileSystemAccessRule ace in acl)
+                    {
+                        Console.WriteLine("{0}Account: {1}", tabs, ace.IdentityReference.Value);
+                        Console.WriteLine("{0}Type: {1}", tabs, ace.AccessControlType);
+                        Console.WriteLine("{0}Rights: {1}", tabs, ace.FileSystemRights);
+                        Console.WriteLine("{0}Inherited: {1}", tabs, ace.IsInherited);
+                        Console.WriteLine();
+                    }
+                    if (curLevel < levels)
+                        GetDirectorySecurity(@directory, curLevel + 1, levels);
+                }
+                catch
+                {
+                    Console.WriteLine("Could not access {0}", directory);
+                }
+            }
+        }
+        static void GetDirectorySecurity(string dir, int curLevel, int levels)
+        {
+            string[] dirs = Directory.GetDirectories(@dir);
+            string tabs = "";
+            for (int i = 0; i < curLevel; i++)
+                tabs += "\t";
+            foreach (string directory in dirs)
+            {
+                Console.WriteLine(tabs.Substring(0, tabs.Length - 1) + "---------------------------------------------------------");
+                Console.WriteLine(tabs.Substring(0, tabs.Length - 1) + directory);
+                try
+                {
+                    DirectoryInfo dInfo = new DirectoryInfo(directory);
+                    DirectorySecurity dSecurity = dInfo.GetAccessControl();
+                    AuthorizationRuleCollection acl = dSecurity.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+                    foreach (FileSystemAccessRule ace in acl)
+                    {
+                        Console.WriteLine("{0}Account: {1}", tabs, ace.IdentityReference.Value);
+                        Console.WriteLine("{0}Type: {1}", tabs, ace.AccessControlType);
+                        Console.WriteLine("{0}Rights: {1}", tabs, ace.FileSystemRights);
+                        Console.WriteLine("{0}Inherited: {1}", tabs, ace.IsInherited);
+                        Console.WriteLine();
+                    }
+                    if (curLevel < levels)
+                        GetDirectorySecurity(@directory, curLevel + 1, levels);
+                }
+                catch
+                {
+                    Console.WriteLine("Could not access {0}", directory);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            GetDirectorySecurity("S:\\Faculty-of-Medicine-and-Health\\Anatomy", 4);
+
+            /*try
+            {
+                if (args[0] != null && args[1] != null)
+                    GetDirectorySecurity(@args[0], int.Parse(args[1]));
+                else
+                {
+                    Console.WriteLine("This program requires the input of a starting directory path");
+                    Console.WriteLine("including the letter drive followed by an integer specifying");
+                    Console.WriteLine("how many directories deep to recursively scan");
+                    Console.WriteLine();
+                    Console.WriteLine("Example: programname C:\\shared\\ 2");
+                }
+            }
+            catch
+            {
+                {
+                    Console.WriteLine("This program requires the input of a starting directory path");
+                    Console.WriteLine("including the letter drive followed by an integer specifying");
+                    Console.WriteLine("how many directories deep to recursively scan");
+                    Console.WriteLine();
+                    Console.WriteLine("Example: FolderPermissions C:\\shared\\ 2");
+                    Console.WriteLine("\tThis will scan 2 levels deep on the shared folder on C:");
+                }
+            }*/
+            Console.ReadKey();
+        }
+    }
+}
